b479285fab568e0403b7f117cee1aa88660e8441
ogeeDeveloper <omarogrant202@hotmail.com>
2021-10-13 16:17:25 -0500

    Added Material UI ad made the app Navigation

--- src/components/MainLayout/NavigationBar.js
+++ src/components/MainLayout/NavigationBar.js
@@ -1,7 +1,34 @@
+import {AppBar, Typography} from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles"
+import { Link } from 'react-router-dom'
+
+//Create a stylesheet using Material UI makeStyle Libray
+const useStyles = makeStyles(theme=>({
+    AppBar: {
+        backgroundColor: 'blue'
+    },
+    link: {
+        textDecoration: 'none'
+    },
+    title:{
+        color: 'white',
+        cursor: 'pointer'
+    }
+
+}))
+
 const NavigationBar = () => {
+    //iitialie the stling from the useStyles
+    const classes = useStyles()
+
     return (
         <div>
-            Navigation
+            <AppBar className={classes.AppBar} position="fixed" variant="h6">
+                <Link to="/" className={classes.link}>
+                    {/* Typograp for stlyling the text in the links */}
+                    <Typography className={classes.title}>PokeInfo</Typography>
+                </Link>
+            </AppBar>
         </div>
     )
 }
